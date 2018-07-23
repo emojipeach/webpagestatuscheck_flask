15aca03b33d509adc92a1153de1676dfe3b4ce89
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-07-23 22:50:02 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Interval to refresh status codes in seconds
+refresh_interval = 60.0
+
+# File containing groups ofurls to check in json format. See included example 'checkurls.json'
+filename = 'checkurls.json'
+
+# Message to display if sites are not connectable
+site_down = 'UNREACHABLE'
