7098e404acbc7842e91dd6fd143bdbba2ac12bd6
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-07-24 21:46:25 +0000

    Added number_threads as a variable

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
-# Interval to refresh status codes in seconds
+""" Interval to refresh status codes in seconds. """
 refresh_interval = 60.0
 
-# File containing groups ofurls to check in json format. See included example 'checkurls.json'
+""" File containing groups ofurls to check in json format. See included example 'checkurls.json'. """
 filename = 'checkurls.json'
 
-# Message to display if sites are not connectable
+""" Message to display if sites are not connectable. """
 site_down = 'UNREACHABLE'
+
+""" Number of concurrent connections while checking sites. """
+number_threads = 8
