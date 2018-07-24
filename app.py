c2f207ec56e4750ea964be89600e6d77d929607b
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-07-24 21:45:22 +0000

    Refactored check_multiple_urls function to make it testable.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 from flask import Flask, render_template, jsonify
 from time import gmtime, strftime
 
-from settings import refresh_interval, filename, site_down
+from settings import refresh_interval, filename, site_down, number_threads
 
 
 def is_reachable(url):
@@ -32,32 +32,38 @@ def get_status_code(url):
 
 def check_single_url(url):
     """This function checks a single url and if connectable returns
-    the status code, else returns UNREACHABLE."""
+    the status code, else returns variable site_down (default: UNREACHABLE)."""
     if is_reachable(urlparse(url).hostname) == True:
         return str(get_status_code(url))
     else:
         return site_down
 
 
+def launch_checker():
+    """This function launches the check_multiple_urls function every x seconds
+    (defined in refresh interval variable)."""
+    t = threading.Timer
+    t(refresh_interval, launch_checker).start()
+    global returned_statuses
+    returned_statuses = check_multiple_urls()
+
 def check_multiple_urls():
-    """ This function checks through urls specified in the checkurls.json file
-    and returns their statuses as a dictionary every 60s."""
+    """This function checks through urls specified in the checkurls.json file
+    (specified in the filename variable) and
+    returns their statuses as a dictionary."""
     statuses = {}
     temp_list_urls = []
     temp_list_statuses = []
-    global returned_statuses
     global last_update_time
-    t = threading.Timer
-    t(refresh_interval, check_multiple_urls).start()
     for group, urls in checkurls.items():
         for url in urls:
             temp_list_urls.append(url)
-    pool = ThreadPool(8)
+    pool = ThreadPool(number_threads)
     temp_list_statuses = pool.map(check_single_url, temp_list_urls)
     for i in range(len(temp_list_urls)):
         statuses[temp_list_urls[i]] = temp_list_statuses[i]
     last_update_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    returned_statuses = statuses
+    return statuses
 
 
 app = Flask(__name__)
@@ -87,5 +93,5 @@ last_update_time = 'time string'
 
 
 if __name__ == '__main__':
-    check_multiple_urls()
+    launch_checker()
     app.run()
