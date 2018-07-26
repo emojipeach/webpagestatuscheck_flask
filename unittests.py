c0c5432b62c34204afe23ddc5e53c036c4590240
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-07-26 17:54:00 +0000

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -36,6 +36,7 @@ class CheckSingleURLTestCase(unittest.TestCase):
         result = app.check_single_url('https://ksjsjsbdk.ievrygqlsp.com')
         self.assertEqual(result, site_down)
     
+    @unittest.skip("takes too long, reenable if necessary")
     def test_timeout_url(self):
         result = app.check_single_url('https://www.bbc.co.uk:90')
         self.assertEqual(result, site_down)
@@ -59,6 +60,8 @@ class CheckMultipleURLsTestCase(unittest.TestCase):
         "http://localhost:8080"
         ]
 }
+        app.list_urls = app.generate_list_urls(app.checkurls)
+        
         expected = {
 "https://www.bbc.co.uk": "200",
 "http://doesnotexist.bbc.co.uk": "UNREACHABLE",
@@ -68,5 +71,79 @@ class CheckMultipleURLsTestCase(unittest.TestCase):
 }
         result = app.check_multiple_urls()
         self.assertEqual(result, expected)
+        
+class CompareSubmittedTestCase(unittest.TestCase):
+    """Tests the compare_submitted function."""
+    
+    def test_known_submitted_url(self):
+        app.checkurls = {
+"BBC": [
+        "https://www.bbc.co.uk", 
+        "http://doesnotexist.bbc.co.uk",
+        "https://www.bbc.co.uk/404"
+        ],
+"Google": [
+        "https://www.google.com",
+        "http://localhost:8080"
+        ]
+}
+        submitted = 'https://www.bbc.co.uk'
+        result = app.compare_submitted(submitted)
+        self.assertEqual(submitted, result[1])
+        self.assertTrue(result[0])
+        
+    def test_unknown_submitted_url(self):
+        app.checkurls = {
+"BBC": [
+        "https://www.bbc.co.uk", 
+        "http://doesnotexist.bbc.co.uk",
+        "https://www.bbc.co.uk/404"
+        ],
+"Google": [
+        "https://www.google.com",
+        "http://localhost:8080"
+        ]
+}
+        submitted = 'https://unknown.com'
+        result = app.compare_submitted(submitted)
+        self.assertEqual(submitted, result[1])
+        self.assertFalse(result[0])
+    
+    def test_whitespace_stripping_submitted_url(self):
+        app.checkurls = {
+"BBC": [
+        "https://www.bbc.co.uk", 
+        "http://doesnotexist.bbc.co.uk",
+        "https://www.bbc.co.uk/404"
+        ],
+"Google": [
+        "https://www.google.com",
+        "http://localhost:8080"
+        ]
+}
+        submitted = '  https://www.bbc.co.uk   '
+        result = app.compare_submitted(submitted)
+        self.assertEqual('https://www.bbc.co.uk', result[1])
+        self.assertTrue(result[0])
+
+class HTTPSStartStripTestCase(unittest.TestCase):
+    """Tests the https_start_strip function."""
+    
+    def test_google_url(self):
+        result = app.https_start_strip('https://www.google.com')
+        self.assertEqual(result, 'https://www.google.com')
+    
+    def test_whitespace_url(self):
+        result = app.https_start_strip('       https://www.google.com    ')
+        self.assertEqual(result, 'https://www.google.com')
+
+    def test_no_https_url(self):
+        result = app.https_start_strip('www.google.com')
+        self.assertEqual(result, 'https://www.google.com')
+    
+    def test_uppercase_url(self):
+        result = app.https_start_strip('HTTPS://WWW.GOOGLE.COM')
+        self.assertEqual(result, 'https://www.google.com')
+
 
 unittest.main()
